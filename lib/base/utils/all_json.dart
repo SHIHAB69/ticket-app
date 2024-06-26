42ae9e49126d85aee5c14d99a9a097d201adfd8c
SHIHAB69 <southcybersoldier@gmail.com>
2024-06-27 03:46:53 +0600

    day-5

---
+++ lib/base/utils/all_json.dart
@@ -0,0 +1,70 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New - Yourk"
+    },
+    'to':{
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': "8H 30M",
+    'date': "1 May",
+    'depurture_time': "8:00 AM",
+    "number": 23
+  },
+  {
+    'from': {
+      'code': "DHK",
+      'name': "Dhaka"
+    },
+    'to':{
+      'code': "SH",
+      'name': "Shanghai"
+    },
+    'flying_time': "4H 20M",
+    'date': "1 May",
+    'depurture_time': "9:00 AM",
+    "number": 45
+  },{
+    'from': {
+      'code': "NYC",
+      'name': "New - Yourk"
+    },
+    'to':{
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': "8H 30M",
+    'date': "1 May",
+    'depurture_time': "8:00 AM",
+    "number": 23
+  },
+  {
+    'from': {
+      'code': "DHK",
+      'name': "Dhaka"
+    },
+    'to':{
+      'code': "SH",
+      'name': "Shanghai"
+    },
+    'flying_time': "4H 20M",
+    'date': "1 May",
+    'depurture_time': "9:00 AM",
+    "number": 45
+  }, {
+    'from': {
+      'code': "NYC",
+      'name': "New - Yourk"
+    },
+    'to':{
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': "8H 30M",
+    'date': "1 May",
+    'depurture_time': "8 AM",
+    "number": 55
+  },
+];
--- lib/base/widgets/app_double_text.dart
+++ lib/base/widgets/app_double_text.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:ielts_test/base/res/styles/app_styles.dart';
+import 'package:ielts_test/screens/all_tickets.dart';
 
 class AppDoubleText extends StatelessWidget {
-  const AppDoubleText({super.key, required this.bigText, required this.smallText});
+  const AppDoubleText(
+      {super.key, required this.bigText, required this.smallText});
+
   final String bigText;
   final String smallText;
+
   @override
   Widget build(BuildContext context) {
-    return Container(height: 40, width: 40, color: Colors.redAccent,);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          bigText,
+          style: AppStyles.headlineStyle2,
+        ),
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => const AllTickets(),
+              ),
+            );
+          },
+          child: Text(
+            smallText,
+            style: AppStyles.textStyle.copyWith(color: AppStyles.primaryColor),
+          ),
+        )
+      ],
+    );
   }
 }
